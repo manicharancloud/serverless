ab498dfa962efa230b8eae3659d1bf2d3deda99b
Mani Charan <loka.m@northeastern.edu>
2023-11-28 15:32:17 -0500

    added code to fetch AWS Secrets

--- lambda.py
+++ lambda.py
@@ -8,27 +8,19 @@ from google.cloud import storage
 
 def lambda_handler(event, context):
 
-    # Google Cloud Storage information
-    gcs_bucket_name = "bucket-for-lambda-f93b680"
-
-    # AWS Lambda temporary directory
+    gcs_bucket_name = os.environ['bucketName']
     temp_dir = '/tmp'
 
     try:
-        # Create a temporary directory to store the downloaded release
         temp_download_dir = tempfile.mkdtemp(dir=temp_dir)
 
-        # Get the URL for the latest code on the default branch
-        submission_url = "https://github.com/tparikh/myrepo/archive/refs/tags/v1.0.0.zip"
-
         sns_message = json.loads(event['Records'][0]['Sns']['Message'])
-
-        # Extract userid and email from the SNS message
         userid = sns_message.get('userid')
         email = sns_message.get('email')
         assignmentid = sns_message.get('assignmentid')
+        # submission_url = sns_message.get('submission_url')
+        submission_url = "https://github.com/tparikh/myrepo/archive/refs/tags/v1.0.0.zip"
 
-        # Your code here using userid and email
         print(f"Received SNS message - UserID: {userid}, Email: {email}, for Assignment: {assignmentid}")
 
         secrets_manager = boto3.client('secretsmanager', region_name='us-east-1')
@@ -48,8 +40,6 @@ def lambda_handler(event, context):
                 for chunk in response.iter_content(chunk_size=8192):
                     file.write(chunk)
 
-        # Upload the ZIP file to Google Cloud Storage
-
         upload_to_gcs(zip_file_path, gcs_bucket_name,storage_client, f"{assignmentid}/{userid}/submission.zip")
 
     except Exception as e:
@@ -57,22 +47,16 @@ def lambda_handler(event, context):
 
     finally:
         print("Here in finally! Cleaning up the tempDirectory")
-        # Clean up temporary directory
         cleanup_temp_dir(temp_download_dir)
 
 def upload_to_gcs(source_path, bucket_name,storage_client, destination_blob_name):
-    """Uploads a file to Google Cloud Storage."""
-    # storage_client = storage.Client()
 
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
     print("uploading to bucket")
-
-    # Upload the file
     blob.upload_from_filename(source_path)
 
 def cleanup_temp_dir(temp_dir):
-    """Clean up the temporary directory."""
     if os.path.exists(temp_dir):
         for root, dirs, files in os.walk(temp_dir, topdown=False):
             for file in files:
